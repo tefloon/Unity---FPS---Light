546d39571cca9dc81491c8e41ddde0fd6855d73c
Antoni Gawlikowski <tefloon@gmail.com>
2021-02-01 00:30:36 +0100

    Refactoring - zmiana nazw materiałów. Dodane animacje chowania i pokazywania broni. Dodana logika chowania broni przy rzucie granatem.

---
+++ Assets/GAME/Scripts/Bron_Efekty.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Bron_Efekty : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem efektWystrzalu;
+    [SerializeField] private Transform koniecLufy;
+    
+    private Animator animator;
+    private ParticleSystem blysk;
+
+    public void PokazEfektStrzalu()
+	{
+        print("Pokazuje!");
+        blysk.Play();
+
+        animator.SetTrigger("Strzel");
+    }
+
+    public void Przeladuj()
+	{
+        animator.SetTrigger("Przeladuj");
+    }
+
+    public void SchowajBron()
+	{
+        animator.SetTrigger("SchowajBron");
+    }
+
+    public void PokazBron()
+    {
+        animator.SetTrigger("PokazBron");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blysk = Instantiate(efektWystrzalu, koniecLufy.position, koniecLufy.rotation, transform);
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
