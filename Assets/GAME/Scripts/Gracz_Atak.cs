278f3c0a7ef84d488990f044269fe990e2e950de
Antoni Gawlikowski <tefloon@gmail.com>
2021-01-31 19:33:54 +0100

    Strzelanie Raycast-em. Efekt Trafienia. Przeładowanie. Dźwięk strzału oraz przeładowania.

---
+++ Assets/GAME/Scripts/Gracz_Atak.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Gracz_Atak : MonoBehaviour
+{
+	[SerializeField] private float odstepStrzalow;
+	[SerializeField] Transform miejsceStrzalu;
+	[SerializeField] private GameObject efektTrafienia;
+	
+	[SerializeField] private AudioClip dzwiekStrzalu;
+	[SerializeField] private AudioClip dzwiekPrzeladowania;
+
+	[SerializeField] private int maxAmmo;
+	[SerializeField] private float dlugoscPrzeladowania;
+
+	private int obecneAmmo;
+	private bool czyPrzeladowuje;
+
+	private float nastepnyStrzal = 0;
+	private AudioSource odtwarzacz;
+
+	private void Start()
+	{
+		obecneAmmo = maxAmmo;
+		odtwarzacz = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (CzyMoznaStrzelac())
+		{
+			if (Input.GetButton("Fire1") && Time.time >= nastepnyStrzal)
+			{
+				print("Strzelam!");
+				StrzalRaycast();
+				nastepnyStrzal = Time.time + odstepStrzalow;
+				obecneAmmo -= 1;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.R) && !czyPrzeladowuje)
+		{
+			StartCoroutine(Przeladuj());
+		}
+	}
+
+	IEnumerator Przeladuj()
+	{
+		print("Prze³adowujê...");
+
+		czyPrzeladowuje = true;
+		odtwarzacz.PlayOneShot(dzwiekPrzeladowania);
+
+		yield return new WaitForSeconds(dlugoscPrzeladowania);	
+		obecneAmmo = maxAmmo;
+
+		czyPrzeladowuje = false;
+		print("Prze³adowanie wykonane!");
+	}
+
+	bool CzyMoznaStrzelac()
+	{
+		if (obecneAmmo <= 0) return false;
+		if (czyPrzeladowuje) return false;
+
+		return true;
+	}
+
+	void StrzalRaycast()
+	{
+		RaycastHit cel;
+
+		if (Physics.Raycast(miejsceStrzalu.position, miejsceStrzalu.forward, out cel))
+		{
+			Debug.DrawRay(miejsceStrzalu.position, miejsceStrzalu.forward * 50f, Color.green, 2f);
+			var go = Instantiate(efektTrafienia, cel.point, Quaternion.LookRotation(cel.normal));
+			// print(cel.transform.name);
+		}
+
+		odtwarzacz.PlayOneShot(dzwiekStrzalu);
+	}
+}
